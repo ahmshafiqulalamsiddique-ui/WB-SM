97d2f18fa24ed9d55d70d907680c77d8ddf98f33
ahmshafiqulalamsiddique-ui <ahmshafiqulalamsiddique@gmail.com>
2025-09-24 18:38:31 +0600

    Fix MySQL port type to number

--- src/lib/mysql.ts
+++ src/lib/mysql.ts
@@ -6,7 +6,7 @@ const MYSQL_CONFIG = {
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DATABASE || 'taleskillz_db',
-  port: process.env.MYSQL_PORT || 3306,
+  port: parseInt(process.env.MYSQL_PORT || '3306'),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
